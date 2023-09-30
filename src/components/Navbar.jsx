2665f035dad74eac159f821807d1cc05be565f01
Alper <alper.demir.fener@gmail.com>
2023-09-30 20:53:11 +0300

    css düzenlemeleri

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,6 +19,16 @@ const Navbar = () => {
         }
     }
 
+    const dropDownSettings = () => {
+        const settings = document.getElementById("settings");
+        if(settings.classList.contains("hidden")){
+            settings.classList.remove("hidden")
+        }
+        else{
+            settings.classList.add("hidden")
+        }
+    }
+    
     return (
         <div className='h-[74px] w-[81%] mx-auto max-[1247px]:w-full'>
             <header className='flex justify-around items-center h-full max-[1247px]:justify-between max-[1247px]:mx-5 max-[700px]:justify-center'>
@@ -53,19 +63,28 @@ const Navbar = () => {
 
                     </ul>
                 </div>
-
-                <div className="more group cursor-pointer max-[700px]:absolute max-[700px]:top-[25px] max-[700px]:right-5">
-                    <svg aria-label="Daha Fazla" class="text-[#b8b8b8] group-hover:text-black transition-colors duration-300" fill="rgb(184, 184, 184)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Daha Fazla</title><rect fill="currentColor" height="2.5" rx="1.25" width="21" x="3" y="7"></rect><rect fill="currentColor" height="2.5" rx="1.25" width="14" x="10" y="15"></rect></svg>
+                
+                {/* settings-dropdown */}
+                <div className="group cursor-pointer max-[700px]:absolute max-[700px]:top-[25px] max-[700px]:right-5 relative">
+                    <svg onClick={dropDownSettings} aria-label="Daha Fazla" class="text-[#b8b8b8] group-hover:text-black transition-colors duration-300" fill="rgb(184, 184, 184)" height="24" role="img" viewBox="0 0 24 24" width="24"><title>Daha Fazla</title><rect fill="currentColor" height="2.5" rx="1.25" width="21" x="3" y="7"></rect><rect fill="currentColor" height="2.5" rx="1.25" width="14" x="10" y="15"></rect></svg>
+                    <div id='settings' className='absolute top-7 -left-[8.5rem] border-[1px] border-gray-100 rounded-xl w-[174px] font-semibold text-[15px] hidden'>
+                        <ul className='flex flex-col'>
+                            <li className='p-3 border-b-[1px]'>Görünümü değiştir</li>
+                            <li className='p-3 border-b-[1px]'>Hakkında</li>
+                            <li className='p-3 border-b-[1px]'>Sorun bilir</li>
+                            <li className='p-3'>Çıkış yap</li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div className='fixed bottom-8 left-10 hover:scale-105 duration-200 cursor-pointer flex w-[40%] justify-center max-[700px]:justify-center max-[700px]:left-0 max-[700px]:flex max-[700px]:w-full max-[700px]:bottom-28'>
-                    <div className='flex items-center gap-2 rounded-full px-6 py-4 font-semibold text-sm border-[1px] border-gray-100 shadow-md' onClick={changePostStatus}>
+                <div className='fixed bottom-8 left-10 hover:scale-105 duration-200 cursor-pointer flex w-[40%] justify-center max-[700px]:justify-center max-[700px]:left-0 max-[700px]:flex max-[700px]:w-full max-[700px]:bottom-24 z-10'>
+                    <div className='flex items-center gap-2 rounded-full px-6 py-4 font-semibold text-sm border-[1px] border-gray-100 shadow-md bg-white' onClick={changePostStatus}>
                         <span>{post}</span> <svg aria-hidden="true" className='text-[#b8b8b8]' fill="none" height="16" viewBox="0 0 12 16" width="12" xmlns="http://www.w3.org/2000/svg"><path d="M0.640625 11.0645C0.640625 10.8594 0.722656 10.6484 0.86914 10.5078L4.16211 7.25586C4.31445 7.10937 4.51367 7.02148 4.70703 7.02148C5.14062 7.02148 5.43359 7.33203 5.43359 7.74805C5.43359 7.97656 5.3457 8.14062 5.20508 8.27539L3.86914 9.57617L2.91406 10.373L4.25 10.3145L11.041 10.3145C11.498 10.3145 11.8145 10.6191 11.8145 11.0645C11.8145 11.5098 11.498 11.8145 11.041 11.8145L4.25 11.8145L2.91406 11.7559L3.86914 12.5527L5.20508 13.8535C5.3457 13.9883 5.43359 14.1523 5.43359 14.3809C5.43359 14.7969 5.14062 15.1074 4.70703 15.1074C4.51367 15.1074 4.31445 15.0195 4.16211 14.873L0.86914 11.6211C0.722656 11.4746 0.640625 11.2695 0.640625 11.0645ZM0.640625 4.92969C0.640625 4.48437 0.957031 4.17969 1.41406 4.17969L8.20508 4.17969L9.54102 4.23828L8.58594 3.44141L7.25 2.14648C7.10937 2.00586 7.02148 1.8418 7.02148 1.61328C7.02148 1.19727 7.31445 0.892578 7.74805 0.892578C7.94141 0.892578 8.14062 0.980469 8.29297 1.12695L11.5859 4.37305C11.7324 4.51953 11.8145 4.72461 11.8145 4.92969C11.8145 5.13477 11.7324 5.3457 11.5859 5.48633L8.29297 8.73828C8.14062 8.88477 7.94141 8.97266 7.74805 8.97266C7.31445 8.97266 7.02148 8.66797 7.02148 8.25195C7.02148 8.02344 7.10937 7.85937 7.25 7.72461L8.58594 6.41797L9.54102 5.62109L8.20508 5.68555L1.41406 5.68555C0.957031 5.68555 0.640625 5.375 0.640625 4.92969Z" fill="currentColor"></path></svg>
                     </div>
                 </div>
 
                 {/* bottonav */}
-                <nav className='hidden max-[700px]:block fixed bottom-0 left-0 w-full'>
+                <nav className='hidden max-[700px]:block fixed bottom-0 left-0 w-full bg-white z-10 opacity-95'>
                     <ul className='flex justify-around'>
                         <NavLink to="/" className='hover:bg-[#F5F5F5] py-5 px-8 rounded-md my-[4px] mx-[2px] transition-all duration-500 cursor-pointer max-[700px]:px-4'>
                             {
